58a12c9a4b1c6accdcad11375eeac08b17518eb5
NadegeL <nadege.luthier@gmai.com>
2024-07-19 12:48:26 +0200

    8 et 9

---
+++ ES6_basic/8-getBudgetCurrentYear.js
@@ -0,0 +1,16 @@
+function getCurrentYear() {
+  const date = new Date();
+  return date.getFullYear();
+}
+
+export default function getBudgetForCurrentYear(income, gdp, capita) {
+  const year = getCurrentYear();
+
+  const budget = {
+    income,
+    gdp,
+    capita,
+  };
+
+  return budget;
+}
