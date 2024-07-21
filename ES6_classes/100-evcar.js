a27404d01d24b3df9d95447eaba4e3c130dbed53
NadegeL <nadege.luthier@gmai.com>
2024-07-21 22:34:11 +0200

    range

--- ES6_classes/100-evcar.js
+++ ES6_classes/100-evcar.js
@@ -1,12 +1,21 @@
 import Car from './10-car';
 
 export default class EvCar extends Car {
-  constructor(brand, motor, color, battery) {
+  constructor(brand, motor, color, battery, range) {
     super(brand, motor, color);
     this._battery = battery;
+    this._range = range;
   }
 
+  get battery() {
+    return this._battery;
+  }
+
+  get range() {
+    return this._range;
+  }
+  
   cloneCar() {
-    return new this.constructor(this._brand, this._motor, this._color, this._battery);
+    return new this.constructor(this._brand, this._motor, this._color, this._battery, this._range);
   }
 }
