9f028a0db6ddab8238d068730784ac0412c189be
NadegeL <nadege.luthier@gmai.com>
2024-11-15 11:13:52 +0100

    autres fix

--- ES6_basic/7-getBudgetObject.js
+++ ES6_basic/7-getBudgetObject.js
@@ -1,6 +1,5 @@
 /* Modify the following function’s budget object to simply
- use the object property value shorthand syntax instead. */
-
+use the object property value shorthand syntax instead. */
 export default function getBudgetObject(income, gdp, capita) {
   const budget = {
     income,
