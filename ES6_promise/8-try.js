11b7da547dec0f6f95112cee584e58a8e672fc6f
NadegeL <nadege.luthier@gmai.com>
2024-11-22 10:57:30 +0100

    return in task 8

--- ES6_promise/8-try.js
+++ ES6_promise/8-try.js
@@ -1,11 +1,7 @@
 export default function divideFunction(numerator, denominator) {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      if (denominator === 0) {
-        reject(new Error('cannot divide by 0'));
-      } else {
-        resolve(numerator / denominator);
-      }
-    }, 1000);
-  });
+  if (denominator === 0) {
+    throw new Error('cannot divide by 0');
+  }
+
+  return numerator / denominator;
 }
