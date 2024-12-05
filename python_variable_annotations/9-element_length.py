cb7d12ae0b80c6e5cc68cef19c51e33fc93bcf0f
NadegeL <nadege.luthier@gmai.com>
2024-12-05 14:43:46 +0100

    Iterable Sequence

--- python_variable_annotations/9-element_length.py
+++ python_variable_annotations/9-element_length.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python3
 """annotate a function below"""
-from typing import List, Tuple
+from typing import List, Tuple, Iterable, Sequence
 
 
-def element_length(lst: List[str]) -> List[Tuple[str, int]]:
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ takes a list of tuple with strings and int as argument"""
     return [(i, len(i)) for i in lst]
