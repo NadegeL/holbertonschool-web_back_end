8cb06c67ed5d16bcc70536434400e4d6eef8e1b4
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 <nadege.luthier@gmail.com>
2024-12-12 14:46:52 +0100

    update and find

--- NoSQL/10-update_topics.py
+++ NoSQL/10-update_topics.py
@@ -2,6 +2,8 @@
 """Python function that changes all topics
 of a school document based on the name"""
 
+import pymongo
+
 
 def update_topics(mongo_collection, name, topics):
     """updates all topics of a school document"""
