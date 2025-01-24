902662ff30a9e3726020df8ad7a7e031efaef364
NadegeL <nadege.luthier@gmai.com>
2025-01-24 10:37:33 +0100

    lint

---
+++ Node_JS_basic/6-http_express.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const app = express();
+const PORT = 1245;
+
+app.get('/', (_, res) => {
+    res.send('Hello Holberton School!');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server listening on PORT ${PORT}`);
+});
+
+if (typeof module !== 'undefined' && module.exports) {
+    module.exports = app;
+}
